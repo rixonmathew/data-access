857dd9e6fe3d550e8fccf8ecb0f33093126e56bf
rixonmathew <rixonmathew@gmail.com>
2019-06-07 08:07:37 +0530

    latest from laptop

---
+++ hazelcast-server/src/main/java/com/rixon/learn/spring/data/hazelcast/server/HazelCastServerApplication.java
@@ -0,0 +1,13 @@
+package com.rixon.learn.spring.data.hazelcast.server;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class HazelCastServerApplication {
+
+    public static void main(String[] args) {
+
+        HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance();
+
+    }
+}
