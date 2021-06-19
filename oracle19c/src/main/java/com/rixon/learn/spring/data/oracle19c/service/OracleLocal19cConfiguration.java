6aedaf14127fdcc8a4bdb532fe446bb5bcebda08
rixon <rixonmathew@gmail.com>
2021-06-19 20:13:24 +0530

    updated boot version

---
+++ oracle19c/src/main/java/com/rixon/learn/spring/data/oracle19c/service/OracleLocal19cConfiguration.java
@@ -0,0 +1,8 @@
+package com.rixon.learn.spring.data.oracle19c.service;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OracleLocal19cConfiguration {
+}
