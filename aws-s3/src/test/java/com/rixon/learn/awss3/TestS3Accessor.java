1642362f1ce9abac513ee018d7273192ebcf8115
rixon <rixonmathew@gmail.com>
2022-04-03 07:05:07 +0530

    added tests for S3 access

--- aws-s3/src/test/java/com/rixon/learn/awss3/TestS3Accessor.java
+++ aws-s3/src/test/java/com/rixon/learn/awss3/TestS3Accessor.java
@@ -1,5 +1,6 @@
 package com.rixon.learn.awss3;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,14 +10,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AWSConfig.class)
+
 public class TestS3Accessor {
-////////////////////////////////////////////////////////////////////////////////////////
     @Autowired
     private AWSConfig awsConfig;
     @Test
     @DisplayName("Test upload of CSV file to S3 bucket")
+    @Disabled("To be run locally only")
     public void testS3upload() {
         S3Accessor s3Accessor = new S3Accessor(awsConfig);
         s3Accessor.uploadInstrumentData(100000);
+
     }
 }
