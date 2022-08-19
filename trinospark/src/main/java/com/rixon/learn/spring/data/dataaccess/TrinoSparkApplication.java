d6bbcfb8afabb6b0ea08b1b374f86d164113d6dd
rixon <rixonmathew@gmail.com>
2022-08-19 22:33:50 +0530

    updated spring boot versions

--- trinospark/src/main/java/com/rixon/learn/spring/data/dataaccess/TrinoSparkApplication.java
+++ trinospark/src/main/java/com/rixon/learn/spring/data/dataaccess/TrinoSparkApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrinoSparkApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TrinoSparkApplication.class,args);
     }
