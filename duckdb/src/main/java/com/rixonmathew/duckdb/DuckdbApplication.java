55dc250a07545ad9c0292bfcde7fcd0114312ce9
rixonmathew <rixonmathew@gmail.com>
2024-07-19 08:38:51 +0530

    Add Maven support and new Java classes
    The .gitignore file was created to exclude irrelevant files from the repository. The Maven support was initiated by adding a new pom.xml file, mvnw, mvnw.cmd, and java classes created in the directory src/main/java/com/rixonmathew/duckdb. Furthermore, a Spring Boot test file 'DuckdbApplicationTests' was included in the directory src/test/java/com/rixonmathew/duckdb. All these modifications prepare the project for upcoming development in the Spring framework environment using Java.

---
+++ duckdb/src/main/java/com/rixonmathew/duckdb/DuckdbApplication.java
@@ -0,0 +1,13 @@
+package com.rixonmathew.duckdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckdbApplication.class, args);
+    }
+
+}
---
+++ duckdb/src/main/java/com/rixonmathew/duckdb/DuckdbController.java
@@ -0,0 +1,11 @@
+package com.rixonmathew.duckdb;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DuckdbController {
+
+    public String query(String query) {
+        return query
+    }
+}
