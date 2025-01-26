dceca6051e1c01aa5d54bcb5e23db423926fc20f
rixonmathew <rixonmathew@gmail.com>
2025-01-26 09:29:36 +0530

    added module for kuzudb

---
+++ kuzudb/src/test/java/com/rixonmathew/dataacess/kuzudb/KuzudbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rixonmathew.dataacess.kuzudb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuzudbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
