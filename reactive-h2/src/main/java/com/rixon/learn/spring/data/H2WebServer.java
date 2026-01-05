7bf2fadcd6a7111cbce6e38aa499832a8c4881b8
rixonmathew <578980+rixonmathew@users.noreply.github.com>
2026-01-05 06:23:13 +0530

    Make H2 port configuration dynamic and enable profile-based testing
    - Add `@Value` annotation to make H2 web and TCP ports configurable through properties.
    - Introduce `h2.port` property in test `application.properties`.
    - Update `@SpringBootTest` to use `RANDOM_PORT` and activate test profile.

--- reactive-h2/src/main/java/com/rixon/learn/spring/data/H2WebServer.java
+++ reactive-h2/src/main/java/com/rixon/learn/spring/data/H2WebServer.java
@@ -1,6 +1,8 @@
 package com.rixon.learn.spring.data;
 
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +12,16 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class H2WebServer {
+
+    @Value("${h2.port:29596}")
+    private int port;
     private Server webServer;
     private Server tcpServer;
 
     @EventListener(org.springframework.context.event.ContextRefreshedEvent.class)
     public void start() throws java.sql.SQLException {
-        this.webServer = Server.createWebServer("-webPort", "29596", "-tcpAllowOthers").start();
-        this.tcpServer = Server.createTcpServer("-tcpPort", "29597", "-tcpAllowOthers").start();
+        this.webServer = Server.createWebServer("-webPort", String.valueOf(port), "-tcpAllowOthers").start();
+        this.tcpServer = Server.createTcpServer("-tcpPort", String.valueOf(port+1), "-tcpAllowOthers").start();
     }
 
     @EventListener(org.springframework.context.event.ContextClosedEvent.class)
--- reactive-h2/src/test/java/com/rixon/learn/spring/data/TestReactiveH2Application.java
+++ reactive-h2/src/test/java/com/rixon/learn/spring/data/TestReactiveH2Application.java
@@ -3,8 +3,9 @@ package com.rixon.learn.spring.data;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = ReactiveH2DataAccessApplication.class,webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(classes = ReactiveH2DataAccessApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestReactiveH2Application {
 
     @Test
