13a3261c7c0fd50de5c5b7adfef2a041575e90f7
rixonmathew <rixonmathew@gmail.com>
2026-05-18 06:51:43 +0530

    Upgrade dependencies, refactor H2 WebServer lifecycle with synchronized start/stop, and add initial memory resource files.

--- reactive-h2/src/main/java/com/rixon/learn/spring/data/H2WebServer.java
+++ reactive-h2/src/main/java/com/rixon/learn/spring/data/H2WebServer.java
@@ -2,7 +2,6 @@ package com.rixon.learn.spring.data;
 
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -17,16 +16,29 @@ public class H2WebServer {
     private int port;
     private Server webServer;
     private Server tcpServer;
+    private boolean started = false;
 
     @EventListener(org.springframework.context.event.ContextRefreshedEvent.class)
-    public void start() throws java.sql.SQLException {
+    public synchronized void start() throws java.sql.SQLException {
+        if (started) {
+            return;
+        }
         this.webServer = Server.createWebServer("-webPort", String.valueOf(port), "-tcpAllowOthers").start();
-        this.tcpServer = Server.createTcpServer("-tcpPort", String.valueOf(port+1), "-tcpAllowOthers").start();
+        int tcpPort = port == 0 ? 0 : port + 1;
+        this.tcpServer = Server.createTcpServer("-tcpPort", String.valueOf(tcpPort), "-tcpAllowOthers").start();
+        started = true;
     }
 
     @EventListener(org.springframework.context.event.ContextClosedEvent.class)
-    public void stop() {
-        this.webServer.stop();
-        this.tcpServer.stop();
+    public synchronized void stop() {
+        if (this.webServer != null) {
+            this.webServer.stop();
+            this.webServer = null;
+        }
+        if (this.tcpServer != null) {
+            this.tcpServer.stop();
+            this.tcpServer = null;
+        }
+        started = false;
     }
 }
